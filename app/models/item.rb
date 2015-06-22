557cdbd7ae2e93c10a0c0519aaa9c218bf27ab8f
Salman Ansari <salman.ansari@gmail.com>
2015-06-22 15:25:40 +0200

    order items by most recent

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :list
 
+  default_scope { order('id DESC') }
+
   scope :done, -> { where(done: true) }
   scope :active, -> { where(done: false) }
 end
